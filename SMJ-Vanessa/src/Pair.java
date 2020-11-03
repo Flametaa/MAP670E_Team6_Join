9d023581b8221e62d6805dc82d519653319e7417
almarouk <72821992+almarouk@users.noreply.github.com>
2020-11-03 12:10:19 +0100

    Add hashCode

--- SMJ-Vanessa/src/Pair.java
+++ SMJ-Vanessa/src/Pair.java
@@ -14,6 +14,13 @@ public class Pair<A, B> {
   public B getSecond() {
     return second;
   }
+  
+  @Override
+  public int hashCode() {
+    int hashFirst = first != null ? first.hashCode() : 0;
+    int hashSecond = second != null ? second.hashCode() : 0;
+    return (hashFirst + hashSecond) * hashSecond + hashFirst;
+  }
 
   public boolean equals(Object other) {
     if (!(other instanceof Pair<?, ?>)) {
