e6e4d254a7f0ddc66c5c17133703f0433d56423a
Vanessa Chahwan <vanessa.chahwan@gmail.com>
2020-11-07 23:19:10 +0100

    Update

--- SortMergeJoin/src/main/scala/Join/SMJSpark.scala
+++ SortMergeJoin/src/main/scala/Join/SMJSpark.scala
@@ -3,9 +3,6 @@ package Join
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 import org.apache.spark._
-import java.io._
-import java.nio.file.Files
-import java.nio.file.Paths
 
 object SMJSpark {
   def main(args: Array[String]): Unit = {
@@ -17,10 +14,10 @@ object SMJSpark {
     val inputPath1 = "tables/clients_heavy.csv"
     val inputPath2 = "tables/purchases_heavy.csv"
 
+    val intermediateDir = "intermediate"
     val outputDir = "results"
-    val outputFile = "joined.csv"
-    val outputPath = outputDir + "/" + outputFile
-    if (!Files.exists(Paths.get(outputDir))) Files.createDirectory(Paths.get(outputDir))
+    
+    val outputPath = "results/joined.csv"
 
     val start = System.currentTimeMillis()
 
@@ -36,21 +33,15 @@ object SMJSpark {
 
     val endJoin = System.currentTimeMillis()
 
-    writeRDDToFile(joined, outputPath)
+    FileManager.writeRDDToFile(joined, intermediateDir, outputDir, outputPath)
     val end = System.currentTimeMillis()
 
     val joinDuration = endJoin - start;
     val totalDuration = end - start;
+    
     println(joined.count() + " records")
     println("Join Duration: " + joinDuration + " ms")
     println("Total Duration: " + totalDuration + " ms")
-  }
-
-  def writeRDDToFile(rdd: RDD[Array[String]], outputPath: String) = {
-    val bw = new BufferedWriter(new FileWriter(outputPath))
-    try {
-      rdd.collect().foreach(record => bw.write(record.mkString(",") + "\n"))
-    } finally bw.close()
   }
 }
 
