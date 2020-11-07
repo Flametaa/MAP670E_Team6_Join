b6478da90b5b4df5e2a02965f9d3d79e5b7111ac
HUANG-KX <65315591+HUANG-KX@users.noreply.github.com>
2020-11-08 04:13:42 +0800

    Add files via upload

--- BufferBlock.scala
+++ BufferBlock.scala
@@ -1,25 +1,16 @@
-import org.apache.spark.Partitioner
-import org.apache.spark.rdd.RDD
-
-import scala.reflect.ClassTag
-
 class BufferBlock(TuplePerPageIn: Int = 1000, BlockSizeIn: Int = 10) {
   // simulate the block
   var TuplePerPage: Int = TuplePerPageIn
   var BlockSize: Int = BlockSizeIn
   var times: Int = 0
-  var BlockContentR: RDD[(Any,Any)] = null
-  var BlockContentS: RDD[(Any,Any)] = null
-  var BlockContentO: RDD[(Any,Any)] = null
-  def ReadR[K: ClassTag, V:ClassTag](rdd: RDD[(K,V)]): Unit ={
-    BlockContentR = rdd
+  var BlockContentR: Array[String] = new Array[String](TuplePerPageIn*(BlockSizeIn-2))
+  var BlockContentS: Array[String] = new Array[String](TuplePerPageIn)
+  var BlockContentO: Array[String] = new Array[String](TuplePerPageIn)
+  def ReadR(data: Array[String]): Unit ={
+    BlockContentR = data
   }
-  def ReadS[K: ClassTag, W:ClassTag](rdd: RDD[(K,W)]): Unit ={
-    BlockContentS = rdd
-  }
-  def join[K: ClassTag, V:ClassTag, W:ClassTag](rdd1: RDD[(K,V)], rdd2: RDD[(K,W)],partitioner: Partitioner): RDD[(K,(V,W))] ={
-    BlockContentO = rdd1.cogroup(rdd2,partitioner).flatMapValues(pair => for (v<-pair._1.iterator; w<-pair._2.iterator)yield(v,w))
-    return BlockContentO.take(TuplePerPage)
+  def ReadS(data: Array[String]): Unit ={
+    BlockContentS = data
   }
   def clear(a:Int = 0, b:Int = 0,c:Int = 0): Unit ={
     if (a==1){
