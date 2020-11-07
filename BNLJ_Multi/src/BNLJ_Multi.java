eb252e26e95bcf0e5685f45c0f5b8e76dfa72635
martin <aiacoteros92@gmail.com>
2020-11-07 10:45:16 +0100

    solution plus rapide sigle thread

--- BNLJ_Multi/src/BNLJ_Multi.java
+++ BNLJ_Multi/src/BNLJ_Multi.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,89 +10,108 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BNLJ_Multi {
-    public static void main(String[] args) throws Exception {
-        /*  Tables  */
-        String outerTableFile               = "./data/authors.csv";
-        String innerTableFile               = "./data/posts.csv";
-        String joinTableFile                = "./data/results.csv";
+    public static void main(String[] args) {
+        // Tables 
+        String outerTableFile = "./data/posts.csv";
+        String innerTableFile = "./data/authors.csv";
+        String joinTableFile = "./data/results.csv";
 
-        /** Variables   **/
-        FileReader dataFileInnerRelation    = null;
-        FileReader dataFileOuterRelation    = null;
-        FileWriter dataFileJoinRelation     = null;
-        BufferedReader bufferOuterRelation  = null;
-        BufferedReader bufferInnerRelation  = null;
-        BufferedWriter bufferJoinRelation   = null;
-        int blockSize                       = 64;
-        int bufferSize                      = 1024*blockSize;
-        ArrayList<String> innerBlock        = new ArrayList<String>();
-        ArrayList<String> outerBlock        = new ArrayList<String>();
-        boolean isEmptyBufferOuterRelation  = false;
-        boolean isEmptyBufferInnerRelation  = false;
-        long startTime                      = System.nanoTime();
-        long stopTime                       = 0;
+        // Variables 
+        BufferedReader bufferOuterRelation = null;
+        BufferedReader bufferInnerRelation = null;
+        BufferedWriter bufferJoinRelation = null;
+        int blockSize = 200;
+        int bufferSize = 1024 * blockSize;
+        ArrayList<String> innerBlock = new ArrayList<String>();
+        ArrayList<String> outerBlock = new ArrayList<String>();
+        boolean isEmptyBufferOuterRelation = false;
+        long startTime = System.nanoTime();
+        long stopTime = 0;
 
-        try{
-            dataFileOuterRelation   = new FileReader(outerTableFile);
-            dataFileJoinRelation    = new FileWriter(joinTableFile );
-            bufferOuterRelation     = new BufferedReader(dataFileOuterRelation,bufferSize);
-            bufferJoinRelation      = new BufferedWriter(dataFileJoinRelation );
-
-            while(!isEmptyBufferOuterRelation){
-                isEmptyBufferOuterRelation  = loadBlock(bufferOuterRelation,outerBlock,blockSize);
-                dataFileInnerRelation       = new FileReader(innerTableFile );
-                bufferInnerRelation         = new BufferedReader(dataFileInnerRelation,bufferSize);
-                isEmptyBufferInnerRelation  = false;
-
-                while(!isEmptyBufferInnerRelation){
-                    isEmptyBufferInnerRelation = loadBlock(bufferInnerRelation,innerBlock,blockSize);
-
-                    for (String outerTuple : outerBlock){
-                        for(String innerTuple : innerBlock){
-                            String[] outerTupleFields = outerTuple.split(",");
-                            String[] innerTupleFields = innerTuple.split(",");
-
-                            if(outerTupleFields[0].equals(innerTupleFields[0])){
-                                String joinResult = String.join(",",outerTupleFields[0],outerTupleFields[1],innerTupleFields[2],"\n");
-                                bufferJoinRelation.write(joinResult);
-                            }
-                        }
-                    }
-                }
-
-                if(bufferInnerRelation!=null)   bufferInnerRelation.close();
-                if(dataFileInnerRelation!=null) dataFileInnerRelation.close();
-            }
-
-            //String st = br.lines().collect(Collectors.joining("\n"));
-        }
-        catch(IOException e){
+        try {
+            bufferOuterRelation = new BufferedReader(new FileReader(outerTableFile));
+            bufferJoinRelation = new BufferedWriter(new FileWriter(joinTableFile));
+        } catch (IOException e) {
             System.out.println("Could not read the file...");
             e.printStackTrace();
         }
-        finally{
-            if(bufferOuterRelation!=null)   bufferOuterRelation.close();
-            if(bufferInnerRelation!=null)   bufferInnerRelation.close();
-            if(bufferJoinRelation!=null)    bufferJoinRelation.close();
-            if(dataFileOuterRelation!=null) dataFileOuterRelation.close();
-            if(dataFileInnerRelation!=null) dataFileInnerRelation.close();
-            if(dataFileJoinRelation!=null)  dataFileJoinRelation.close();
+
+        while (!isEmptyBufferOuterRelation) {
+            String innerTuple = null;
+            String[] innerTupleFields = null;
+            try {
+                isEmptyBufferOuterRelation = loadBlock(bufferOuterRelation, outerBlock, blockSize);
+                bufferInnerRelation = new BufferedReader(new FileReader(innerTableFile));
+                innerTuple = bufferInnerRelation.readLine();
+                innerTupleFields = innerTuple.split(",");
+            } catch (FileNotFoundException e2) {
+                // TODO Auto-generated catch block
+                e2.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            //System.out.println(outerBlock.get(0).split(",")[0]);
+            while(innerTuple != null){
+                for (String outerTuple : outerBlock){
+                    String outerTupleFields = outerTuple;
+                    //String[] innerTupleFields = innerTuple.split(",");
+                    
+                    
+                    if(outerTupleFields.equals(innerTupleFields[0]) ){
+                        String joinResult = String.join(",",outerTupleFields,innerTupleFields[2],"\n");
+                        try {
+                            bufferJoinRelation.write(joinResult);
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                try {
+                    innerTuple = bufferInnerRelation.readLine();
+                    if(innerTuple!=null) innerTupleFields = innerTuple.split(",");
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            try {
+                bufferInnerRelation.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
-      
+        
+        try {
+            bufferOuterRelation.close();
+            bufferJoinRelation.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
         stopTime    = System.nanoTime();
         System.out.println("Time: " + TimeUnit.SECONDS.convert(stopTime-startTime,TimeUnit.NANOSECONDS));
     }
 
     public static boolean loadBlock(BufferedReader buffer, ArrayList<String> block, int blockSize) throws IOException {
         String tuple        = buffer.readLine();
-        int tupleNumber     = 1;
+        int tupleNumber     = 0;
         boolean isEmpty     = false;
+        String [] attributes= null;
 
         block.clear();
-        while (tuple != null && tupleNumber <= blockSize){
-            block.add(tuple);
-            if(tupleNumber < blockSize) tuple = buffer.readLine();
+        while (tuple != null && tupleNumber < blockSize){
+            attributes = tuple.split(",");
+            block.add(attributes[0]);
             tupleNumber++;
+            if(tupleNumber < blockSize) tuple = buffer.readLine();
+            
         }
 
         if(tuple == null)
